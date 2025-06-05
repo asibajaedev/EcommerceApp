4e177255475b8b4210322ef73ce7149dd9d8a341
Jose Armando Sibaja Elizondo <sibajaelizondoa@gmail.com>
2025-06-04 21:14:14 -0600

    Agregar archivos de proyecto.

---
+++ Ecommerce.DTO/TarjetaDTO.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecommerce.DTO
+{
+    public class TarjetaDTO
+    {
+        [Required(ErrorMessage = "Ingrese titular")]
+        public string? Titular { get; set; }
+        [Required(ErrorMessage = "Ingrese numero tarjeta")]
+        public string? NumeroTarjeta { get; set; }
+        [Required(ErrorMessage = "Ingrese Vigencia")]
+        public string? FechaVigencia { get; set; }
+        [Required(ErrorMessage = "Ingrese CVV")]
+        public string? CVV { get; set; }
+    }
+}
